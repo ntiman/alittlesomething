ffeb2ce388a60e515b1fadca4a5badb724733bfe
ntiman <ntiman1996@gmail.com>
2024-02-08 17:04:10 -0500

    Prop definition, typings

--- src/components/TailwindTestComponent.vue
+++ src/components/TailwindTestComponent.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-  import {Person} from '@/typings/person';
-  import { defineProps } from "vue"; 
+  // import {Person} from '@/typings/person';
+  import type {PropType} from 'vue';
+  import type {Person} from "../typings/person"
   
-  const people = defineProps<Person[]>()
+  const props = withDefaults(
+    defineProps<{people?: Array<Person>}>(),
+    {
+      people: () => [
+      { "email": "stat@s21", "name": "Stan", "image": ""},
+      { "email": "sasd@s21", "name": "Stan 2", "image": ""},
+      { "email": "stasdat@s21", "name": "Stan 3", "image": ""},
+      ]
+    }
+  )
 
+  function handleChange(event: Event){
+    console.log((event.target as HTMLInputElement).value)
+  }
 </script>
-
 
 <template>
   <h1>AAAAAAAAAA</h1>
---
+++ src/components/base/Button.vue
@@ -0,0 +1,8 @@
+<template>
+<button class="bg-blue-200 text-white p-2 rounded">
+<slot name="icon">
+</slot>
+<span> | </span>
+<slot> Some text </slot>
+</button>
+</template>
